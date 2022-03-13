1749e5a9e4ec6585ef2087bb88e065f8b2b7f5b2
hnz989 <zhenghualin2008@gmail.com>
2022-03-12 21:53:32 -0500

    Finished UI

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
+/*
+1. i removed one of them in every files containing the double semi colon ;;
+    - `\node_modules\semantic-ui-css\components\step.min.css`
+    - `\node_modules\semantic-ui-css\components\step.css`
+    - `\node_modules\semantic-ui-css\semantic.min.css`
+    - `\node_modules\semantic-ui-css\semantic.css`
+2. i have cleaned the cache folders
+    - `\node_modules\.cache\default-development`
+    - `\node_modules\.cache\babel-loader`
+3. i commented all the css rules in /src/App.css (otherwise i got a white page)
+4. restart the dev server (npm start in my case)
+*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import 'semantic-ui-css/semantic.min.css';
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
